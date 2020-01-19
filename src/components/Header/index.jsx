f9b0f6f9144ae3db1102976e3f50e5df7b775bdf
Noel Rosales <noelrosales24@gmail.com>
2020-01-20 01:56:49 +0800

    created header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.scss'
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Brand</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
